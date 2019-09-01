a9ac9f7e7c21f77f858570b5d67085e69f1f3cab
RichardPitts1 <Richardepitts@gmail.com>
2019-09-01 12:32:47 -0500

    -

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,32 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+
+  get '/' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get 'recipes/:id' do
+    @chosenRecipe = Article.find_by_id(params["id"])
+    erb :recipe
+  end 
+
+  patch '/recipes/:id/edit' do
+    erb :edit
+  end 
+
+  post 'recipes/new' do
+    erb :new
+  end
+
+  # Then there is update and delete.  
+
+
+
 end
---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+recipe1 = Recipe.create({
+    name: "name1",
+    ingredients: "ingredients1",
+    cook_time: "cook_time1"
+})
+recipe2 = Recipe.create({
+    name: "name2",
+    ingredients: "ingredients2",
+    cook_time: "cook_time2"
+})
+recipe3 = Recipe.create({
+    name: "name3",
+    ingredients: "ingredients3",
+    cook_time: "cook_time3"
+})
+recipe4 = Recipe.create({
+    name: "name4",
+    ingredients: "ingredients4",
+    cook_time: "cook_time4"
+})
+recipe5 = Recipe.create({
+    name: "name5",
+    ingredients: "ingredients5",
+    cook_time: "cook_time5"
+})
